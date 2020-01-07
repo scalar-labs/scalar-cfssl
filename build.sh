8e2682fa3b1a001d242524956bc9b2996426d2a3
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-01-07 12:53:02 +0900

    Fix versions in build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 set -xe
 
-IMAGE_TAG=1.0.0
+SCALAR_CFSSL_IMAGE_TAG=1.0.0
 CFSSL_GIT_TAG=v1.4.1
 
 rm -rf cfssl
 git clone --depth 1 --branch $CFSSL_GIT_TAG https://github.com/cloudflare/cfssl.git cfssl
 
-docker build cfssl -f cfssl/Dockerfile.alpine -t cloudflare-cfssl:$IMAGE_TAG
+docker build cfssl -f cfssl/Dockerfile.alpine -t cloudflare-cfssl:${CFSSL_GIT_TAG#v}-alpine
 
-docker build . -t scalarlabs/scalar-cfssl:$IMAGE_TAG
+docker build . -t scalarlabs/scalar-cfssl:$SCALAR_CFSSL_IMAGE_TAG
